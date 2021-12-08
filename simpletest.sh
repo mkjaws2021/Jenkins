1efebd9335c3df9231a40d35b719f4989fa73b18
mkjaws2021 <mkj.aws2021@gmail.com>
2021-12-08 12:49:20 +0000

    added simple.sh

--- simpletest.sh
+++ simpletest.sh
@@ -3,6 +3,6 @@ echo "this is start of script"
 echo $MyStringA
 NUM1=0
 NUM2=0
-TOTAL= $(( $NUM1 + $NUM2 ))
+TOTAL=$(( $NUM1 + $NUM2 ))
 echo "Total of" $NUM1 "and" $NUM2 "is" $TOTAL
 echo "complete"
